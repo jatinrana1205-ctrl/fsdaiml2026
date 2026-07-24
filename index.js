269a820f8145a08df1e98eda19222780abaad474
Jatin Singh Rana <jatinrana1205@gmail.com>
2026-07-24 10:14:32 +0530

    jatin

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("hello world")
+function greet(name,callback) {
+    console.log("Hello" + name);
+    callback();
+}
+function sayBye() {
+    console.log("Goodbye");
+}
+greet("Jatin", sayBye);
+
